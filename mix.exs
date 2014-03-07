6a140ad66128e92533b5110198f9207b434f0fb5
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-03-07 23:12:25 +0200

    typo

--- mix.exs
+++ mix.exs
@@ -11,7 +11,6 @@ defmodule RedisPool.Mixfile do
   # Configuration for the OTP application
   def application do
     [
-      registered: [:redis_pool],
       mod: {RedisPool, []},
       applications: [:kernel, :stdlib]
     ]
